d6f84d1edb9fbb3627373ba166b23bbbfd3cd1fd
stephenmoll <stephenmollandco@mail.com>
2023-01-07 21:28:37 +0200

    Prep 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+
+        string grading = "";
+
+        if (grade >= 90)
+        {
+            grading = "A";
+        }
+        else if (grade >= 80)
+        {
+            grading = "B";
+        }
+        else if (grade >= 70)
+        {
+            grading = "C";
+        }    
+        else if (grade >= 60)
+        {
+            grading = "D";
+        }
+        else 
+        {
+            grading = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {grading}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time. ");
+        }
     }
 }
